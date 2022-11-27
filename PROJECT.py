50efa55e50e6b6d1887ca1d705ea24fd4dde3506
mahnoorijaz129 <mahnoorijaz129@gmail.com>
2022-11-27 15:49:36 +0500

    Update PROJECT.py

--- PROJECT.py
+++ PROJECT.py
@@ -3,7 +3,7 @@ opcode = {
     "NEG": '111101',"DIV": '111101',
     "ADD": '000000',"NOT":'111101'}
 
-
+MOD=['00','01','10','11']
 reg0={
     "AL": "000","CL": "001","DL": "010","BL": "011", 
     "AH": "100","CH": "101","DH": "100","BH": "111" }
@@ -18,7 +18,7 @@ mmm={
 
    
 
-print(reg0.keys())
+
 
 operator= input("enter the operator")
 RM= input("enter R/M")
@@ -26,5 +26,5 @@ reg= input("enter register")
 if operator == "MOV":
     if RM=='DX' and reg=='AX':
         if RM in reg0.keys() or RM== reg1.keys():
-            MOD= '11'
-    print(opcode["MOV"],reg1["DX"],reg1["AX"])
+            print('yes')
+    # print(opcode["MOV"],reg1["DX"],reg1["AX"])
