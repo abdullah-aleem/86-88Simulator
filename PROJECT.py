2b82086db8bed6afa34a7e518d085ea67bb7aa28
mahnoorijaz129 <mahnoorijaz129@gmail.com>
2022-12-02 17:29:36 +0500

    UPDATE

--- PROJECT.py
+++ PROJECT.py
@@ -1,6 +1,5 @@
-
 opcode = {
-    "MOV": "100010","SUB": '000101',"XOR":'000110',"INC":'111111',
+    "MOV": '100010',"SUB": '000101',"XOR":'000110',"INC":'111111',
     "NEG": '111101',"DIV": '111101', "MUL":'111101',"OR":'000010', 
     "ADD": '000000',"NOT":'111101',"IMUL":'111101',"IDIV":'111101',
     "DEC":'111111',"":'',"":''}
@@ -13,38 +12,87 @@ reg0={
 reg1={
     "AX": "000","CX": "001","DX": "010","BX": "011", 
     "SP": "100","BP": "101","SI": "100","DI": "111" }
+# contents of memory locations
+M0000=00
+M0001=00
+M0010=00
+M0011=00
+M0100=00
+M0101=00
+M0110=00
+M0111=00
+M1000=00
+M1001=00
+M1010=00
+M1011=00
+M1100=00
+M1101=00
+M1110=00
+M1111=00
+
+# contents of 16 bits registers
+AX=0000
+CX=0000
+DX=0000
+BX=0000
+SP=0000
+BP=0000
+SI=0000
+DI=0000
+
+# contents of 8 bits registers
+AL=00
+CL=00
+DL=00
+BL=00
+AH=00
+CH=00
+DH=00
+BH=00
 
 # mmm={
 #     "[BX+SI]": "000","[BX+DI]": "001","[BP+SI]": "010","[BP+DI]": "011", 
 #     "[SI]": "100","[DI]": "101","[BP]": "100","[BX]": "111" }
 
- 
+bool=False
 inst = input('enter instruction')
 x = inst.split(" ")
 opr =x[0]
 y = x[1]
-z = y.split(",")
-operand1 = z[0]
-operand2 = z[1]
+length=len(inst)
+if length>6:
+    bool=True
+    z = y.split(",")
+    operand1 = z[0]
+    operand2 = z[1]
 
 #         if RM in reg0 or RM in reg1:
 #             MOD= '11'
 #     print(opcode["opr"],reg1["reg"],reg1["RM"])
 
 def Word():
-    global word
-    global dic
-    global flag
+    global word,dic,flag
     flag=True
-    if operand1 in reg0 and operand2 in reg0:
-        word=0
-        dic = reg0
-    elif operand1 in reg1 and operand2 in reg1:
-        word=1
-        dic=reg1
+    if bool==True:
+        if operand1 in reg0 and operand2 in reg0:
+            word=0
+            dic = reg0
+        elif operand1 in reg1 and operand2 in reg1:
+            word=1
+            dic=reg1
+        else:
+            print("invalid operands")
+            flag=False
     else:
-        print("invalid operands")
-        flag=False
+        if y in reg0:
+            word=0
+            dic=reg0
+        elif y in reg1:
+            word=1
+            dic=reg1
+        else:
+            print("invalid operands")
+
 # def Direction():
 #     global D
 #     if operand1[0]=="[":
@@ -60,20 +108,22 @@ if flag:
             case "MOV"|"SUB"|"OR"|"XOR"|"ADD"|"AND":
                 print(opcode[opr],D,word,MOD,dic[operand1],dic[operand2])
             case "NEG":
-                print(opcode[opr],D,word,MOD,'011',dic[operand2])
+                rrr="011"
             case "DIV":
-                print(opcode[opr],D,word,MOD,'110',dic[operand2])
+                rrr='110'
             case "INC":
-                print(opcode[opr],D,word,MOD,"000",dic[operand2])
+                rrr="000"
             case "MUL":
-                print(opcode[opr],D,word,MOD,"100",dic[operand2])
+                rrr="100"
             case "NOT":
-                print(opcode[opr],D,word,MOD,'010',dic[operand2])
+                rrr='010'
             case "IMUL":
-                print(opcode[opr],D,word,MOD,"101",dic[operand2])
+                rrr="101"
             case "IDIV":
-                 print(opcode[opr],D,word,MOD,"111",dic[operand2])
+                rrr="111"
             case "DEC":
-                print(opcode[opr],D,word,MOD,"001",dic[operand2])
-
-            
+                rrr='001'
+    if bool==False:
+        print(opcode[opr],D,word,MOD,rrr,dic[y])
+       
+        
