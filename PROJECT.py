20a80e38aa44111c82728904243c94514c3c14c1
iqrarasool04 <iqrarasool45@gmail.com>
2022-12-02 16:57:12 +0500

    Update PROJECT.py

--- PROJECT.py
+++ PROJECT.py
@@ -52,7 +52,6 @@ from tkinter import *
 window = Tk()
 window.title("Main Window")
 window.geometry("750x600")
-window.state('zoomed')
 window.configure(background='light blue')
 
 def getvals():
@@ -82,26 +81,48 @@ mem3 = Label(window, text='Mem3:', font='ar 9', padx=10)
 mem4 = Label(window, text='Mem4:', font='ar 9', padx=10)
 mem5 = Label(window, text='Mem5:', font='ar 9', padx=10)
 mem6 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem7 = Label(window, text='Mem7:', font='ar 9', padx=10)
+mem8 = Label(window, text='Mem8:', font='ar 9', padx=10)
+
+mem9 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem10 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem11 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem12 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem13 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem14 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem15 = Label(window, text='Mem6:', font='ar 9', padx=10)
+mem16 = Label(window, text='Mem6:', font='ar 9', padx=10)
 
 #Packing fields
-instruction.grid(row=1, column=2)
-output.grid(row=3, column=2)
+instruction.grid(row=2, column=2)
+output.grid(row=4, column=2)
 #Packing registers
-reg1.grid(row=4, column=4)
-reg2.grid(row=5, column=4)
-reg3.grid(row=6, column=4)
-reg4.grid(row=7, column=4)
-reg5.grid(row=8, column=4)
-reg6.grid(row=9, column=4)
-reg7.grid(row=10, column=4)
-reg8.grid(row=11, column=4)
+reg1.grid(row=10, column=5)
+reg2.grid(row=11, column=5)
+reg3.grid(row=12, column=5)
+reg4.grid(row=13, column=5)
+reg5.grid(row=14, column=5)
+reg6.grid(row=15, column=5)
+reg7.grid(row=16, column=5)
+reg8.grid(row=17, column=5)
 #Packing memory locations
-mem1.grid(row=5, column=1)
-mem2.grid(row=6, column=1)
-mem3.grid(row=7, column=1)
-mem4.grid(row=8, column=1)
-mem5.grid(row=9, column=1)
-mem6.grid(row=10, column=1)
+mem1.grid(row=6, column=1, padx=10)
+mem2.grid(row=7, column=1)
+mem3.grid(row=8, column=1)
+mem4.grid(row=9, column=1)
+mem5.grid(row=10, column=1)
+mem6.grid(row=11, column=1)
+mem7.grid(row=12, column=1)
+mem8.grid(row=13, column=1)
+
+mem9.grid(row=14, column=1)
+mem10.grid(row=15, column=1)
+mem11.grid(row=16, column=1)
+mem12.grid(row=17, column=1)
+mem13.grid(row=18, column=1)
+mem14.grid(row=19, column=1)
+mem15.grid(row=20, column=1)
+mem16.grid(row=21, column=1)
 
 #variable to store data
 instructionvalue = StringVar
@@ -122,6 +143,17 @@ mem3value = StringVar
 mem4value = StringVar
 mem5value = StringVar
 mem6value = StringVar
+mem7value = StringVar
+mem8value = StringVar
+
+mem9value = StringVar
+mem10value = StringVar
+mem11value = StringVar
+mem12value = StringVar
+mem13value = StringVar
+mem14value = StringVar
+mem15value = StringVar
+mem16value = StringVar
 
 #Creating entry field
 instructionentry = Entry(window, textvariable=instructionvalue)
@@ -142,29 +174,50 @@ mem3entry = Entry(window, textvariable=mem3value)
 mem4entry = Entry(window, textvariable=mem4value)
 mem5entry = Entry(window, textvariable=mem5value)
 mem6entry = Entry(window, textvariable=mem6value)
+mem7entry = Entry(window, textvariable=mem7value)
+mem8entry = Entry(window, textvariable=mem8value)
+
+mem9entry = Entry(window, textvariable=mem9value)
+mem10entry = Entry(window, textvariable=mem10value)
+mem11entry = Entry(window, textvariable=mem11value)
+mem12entry = Entry(window, textvariable=mem12value)
+mem13entry = Entry(window, textvariable=mem13value)
+mem14entry = Entry(window, textvariable=mem14value)
+mem15entry = Entry(window, textvariable=mem15value)
+mem16entry = Entry(window, textvariable=mem16value)
 
 #Packing entry field
-instructionentry.grid(row=1, column=3)
-outputentry.grid(row=3, column=3)
+instructionentry.grid(row=2, column=3)
+outputentry.grid(row=4, column=3)
 #Packing entry fields for registers
-reg1entry.grid(row=4, column=5)
-reg2entry.grid(row=5, column=5)
-reg3entry.grid(row=6, column=5)
-reg4entry.grid(row=7, column=5)
-reg5entry.grid(row=8, column=5)
-reg6entry.grid(row=9, column=5)
-reg7entry.grid(row=10, column=5)
-reg8entry.grid(row=11, column=5)
+reg1entry.grid(row=10, column=6)
+reg2entry.grid(row=11, column=6)
+reg3entry.grid(row=12, column=6)
+reg4entry.grid(row=13, column=6)
+reg5entry.grid(row=14, column=6)
+reg6entry.grid(row=15, column=6)
+reg7entry.grid(row=16, column=6)
+reg8entry.grid(row=17, column=6)
 #Packing entry fields for memory locations
-mem1entry.grid(row=5, column=2)
-mem2entry.grid(row=6, column=2)
-mem3entry.grid(row=7, column=2)
-mem4entry.grid(row=8, column=2)
-mem5entry.grid(row=9, column=2)
-mem6entry.grid(row=10, column=2)
+mem1entry.grid(row=6, column=2)
+mem2entry.grid(row=7, column=2)
+mem3entry.grid(row=8, column=2)
+mem4entry.grid(row=9, column=2)
+mem5entry.grid(row=10, column=2)
+mem6entry.grid(row=11, column=2)
+mem7entry.grid(row=12, column=2)
+mem8entry.grid(row=13, column=2)
+
+mem9entry.grid(row=14, column=2)
+mem10entry.grid(row=15, column=2)
+mem11entry.grid(row=16, column=2)
+mem12entry.grid(row=17, column=2)
+mem13entry.grid(row=18, column=2)
+mem14entry.grid(row=19, column=2)
+mem15entry.grid(row=20, column=2)
+mem16entry.grid(row=21, column=2)
 
 #Creating conversion button
-Button(text='Convert', command=getvals).grid(row=2, column=3)
+Button(text='Convert', command=getvals).grid(row=3, column=3)
 window.mainloop()
-
 
